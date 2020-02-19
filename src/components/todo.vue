f3614ab9bb039e9a1700eb2976bb89e77dbe9553
January <714365850@qq.com>
2020-02-19 18:49:10 +0800

    commit1

---
+++ src/components/todo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="todo">
+    <div class="title">{{header}}</div>
+    <div class="add">
+      <input type="text" />
+        <img alt="addBt" src="../assets/add.png" @click="handerAdd"/>
+    </div>
+    <ul class="unfinished">
+      <li v-for="item in unfinishedTodos" @click="change(item)" :key="item" class>
+        <input type="radio" :checked="item.status === 'finished'" />
+        <label>{{ item.desc }}</label>
+      </li>
+    </ul>
+    <ul class="finished">
+      <li v-for="item in finishedTodos" :key="item">
+        <input @click="change(item)" type="radio" id="index" :checked="item.status === 'finished'" />
+        <label for="index">{{ item.desc }}</label>
+      </li>
+    </ul>
+
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+export default class HelloWorld extends Vue {}
+</script>
+<style lang="less">
+</style>
